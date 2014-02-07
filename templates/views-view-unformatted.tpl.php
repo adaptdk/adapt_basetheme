45ad98c7a0c344930304c0027f9dc74736a20569
Sam Spinoy <sam@adapt.dk>
2014-02-07 09:21:39 +0100

    adjust views tpl

--- templates/views-view-unformatted.tpl.php
+++ templates/views-view-unformatted.tpl.php
@@ -12,7 +12,11 @@
     <h2><?php print $title; ?></h2>
   <?php endif; ?>
   <?php foreach ($rows as $id => $row): ?>
-    <article <?php if ($classes_array[$id]) { print 'class="' . $classes_array[$id] .'"';  } ?>>
+    <?php if ($classes_array[$id]): ?>
+      <article class="<?php print $classes_array[$id]; ?>">
+    <?php else: ?>
+      <article>
+    <?php endif; ?>
       <?php print $row; ?>
     </article>
   <?php endforeach; ?>
